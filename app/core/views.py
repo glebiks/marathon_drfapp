01468f65d85734f5cc27f48a7870aad14e04c98a
Глеб Рахимжанов <cubicscience@yandex.ru>
2023-08-16 13:31:09 +0300

    little changes

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+# CRUD operaions
+
+class ListToDo(generics.ListAPIView):  # Read
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class DetailToDo(generics.RetrieveAPIView):  # Update
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class CreateToDo(generics.CreateAPIView):  # Create
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class DeleteToDo(generics.CreateAPIView):  # Delete
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
