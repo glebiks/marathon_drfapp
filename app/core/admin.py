09b1eb4593711d550866c85e4ef533850afa44d1
g.rahimzhanov <cubicscience@yandex.ru>
2023-08-21 11:43:56 +0300

    admin panels names added

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(MainTask)
-admin.site.register(SubTask)
-admin.site.register(FullName)
+@admin.register(MainTask)
+class MainTaskAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(FullName)
+class FullNameAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+
